b7ffcde2fa6a6bdd54020150768df3fc6aba8e6b
Azfar <azfarrazzaq23@gmail.com>
2025-04-26 15:52:33 +0500

    modified ScrollingText component

--- app/components/ScrollingText.tsx
+++ app/components/ScrollingText.tsx
@@ -1,23 +1,22 @@
-import "./ScrollingText.css"
-import { useEffect } from "react"
+import "./ScrollingText.css";
+import { useEffect } from "react";
 
+export default function ScrollingText({ text }: { text: string }) {
+  useEffect(() => {
+    let component = document.querySelector(".scrollingText");
+    if (component) {
+      const elem = component.cloneNode(true);
+      const container = document.querySelector(".scrollingTextContainer");
+      if (!container) {
+        throw new Error("scrollingTextContainer not found");
+      }
+      container.append(elem);
+    }
+  }, []);
 
-export default function ScrollingText(props){
-
-    useEffect(()=>{
-        let component = document.querySelector('.scrollingText');
-        if(component){
-            const elem = component.cloneNode(true)
-            document.querySelector(".scrollingTextContainer").append(elem)  
-        }
-    },[]) 
-
-
-    return(
-        <div  className='scrollingTextContainer'>
-            <div  className='scrollingText'>{props.text}</div>
+  return (
+    <div className="scrollingTextContainer">
+      <div className="scrollingText">{text}</div>
     </div>
-
-    )
-
+  );
 }
