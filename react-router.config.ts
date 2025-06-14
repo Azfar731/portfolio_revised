9d120ed66a7d91e4b92a50aa3e7ccd7b511c19d9
Azfar Razzaq <azfarrazzaq23@gmail.com>
2025-06-14 15:32:28 +0500

    made updates to react config

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,7 +4,5 @@ export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: false,
-  async prerender() {
-    return ["/"];
-  },
+  prerender: ["/"],
 } satisfies Config;
