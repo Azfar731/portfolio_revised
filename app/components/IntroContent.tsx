c99692b4ea550858c9956ba6ddf0c4c5c92288b8
Azfar Razzaq <azfarrazzaq23@gmail.com>
2026-02-04 15:41:14 +0500

    feat: Added image in the top section
    Following files were modified:
    - IntroContent.css
    - Introduction.css
    - app.css(only redialGradient class)

--- app/components/IntroContent.tsx
+++ app/components/IntroContent.tsx
@@ -1,5 +1,6 @@
 import "./IntroContent.css";
 import WhatsAppButton from "../common/WhatsAppButton";
+import portfolioImg from "../../public/portfolio_img.jpeg";
 
 export default function IntroContent({
   introContent,
@@ -12,7 +13,7 @@ export default function IntroContent({
 }) {
   return (
     <div className="introContainer">
-      <div id="profileImageContainer"></div>
+      <div id="profileImageContainer"><img src={portfolioImg} alt="Profile" /></div>
       <div id="intro-Content">
         <h2 className="smallText">{introContent.jobTitle}</h2>
         <h1>{introContent.sellingPoint}</h1>
